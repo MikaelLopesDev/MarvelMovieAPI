63d96f8019d34cf375ebb203c5c9fe9794f16b07
MikaelLopesDev <mikaelslopesDev@gmail.com>
2024-01-31 21:51:12 -0300

    Last Version of MarvelMovieAPI

---
+++ Controllers/MarvelMoviesController.cs
@@ -0,0 +1,96 @@
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Data.Dtos;
+
+namespace FilmesAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+
+    public class MarvelMoviesController : ControllerBase
+    {
+        private MovieContext _context;
+        private IMapper _mapper;
+
+        public MarvelMoviesController(MovieContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+
+        public IActionResult AddMovie([FromBody] CreateMovieDto movieDto)
+        {
+            Movie movie = _mapper.Map<Movie>(movieDto);
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecoverById), new { Id = movie.Id }, movie);
+
+        }
+
+        [HttpGet]
+
+        public IEnumerable<Movie> RecoverMovies()
+        {
+            return _context.Movies;
+        }
+
+
+        [HttpGet("{id}")]
+
+        public IActionResult RecoverById(int id)
+        {
+
+            Movie movie = _context.Movies.FirstOrDefault(movie => movie.Id == id);
+
+
+            if (movie != null)
+            {
+                ReadMovieDto movieDto = _mapper.Map<ReadMovieDto>(movie);
+                return Ok(movieDto);
+            }
+            return NotFound();
+        }
+
+
+        [HttpPut("{id}")]
+
+        public IActionResult UpdateById(int id, [FromBody] UpdateMovieDto movieDto)
+        {
+            Movie movie = _context.Movies.FirstOrDefault(movie => movie.Id == id);
+
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(movieDto, movie);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult RemoveById(int id)
+        {
+            Movie movie = _context.Movies.FirstOrDefault(movie => movie.Id == id);
+
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(movie);
+            _context.SaveChanges();
+            return NoContent();
+
+
+        }
+    }
+
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Movie
+    {
+        [Key]
+        [Required]  
+        public int Id { get; set; }
+        [Required(ErrorMessage = "The field title is mandatory") ]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "The field Director is mandatory")]
+        public string Director { get; set; }
+
+        [StringLength(30, ErrorMessage = "The gender can't be above 30 characters")]
+        public string Gender { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [Range(1,200,ErrorMessage = "The time of the movie can't be above 200 minutes ")]
+        public int time { get; set; }
+
+       
+
+
+
+    }
+}
